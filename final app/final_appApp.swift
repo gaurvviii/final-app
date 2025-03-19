f5a13cb8166607ec2ebaf90eb4e2f3751fde96b3
Gaurvi <gaurvirana@gmail.com>
2025-03-19 11:34:06 +0530

    Initial Commit

---
+++ final app/final_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  final_appApp.swift
+//  final app
+//
+//  Created by Gaurvi  on 19/03/25.
+//
+
+import SwiftUI
+
+@main
+struct final_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ final appTests/final_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  final_appTests.swift
+//  final appTests
+//
+//  Created by Gaurvi  on 19/03/25.
+//
+
+import Testing
+@testable import final_app
+
+struct final_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
